94365b2221eab91be071b9ffd6b280f8ca6d1de9
Robert Sawko <robertsawko@gmail.com>
2015-06-01 00:06:50 +0100

    Enforce 80 columns in test

--- tests/breakup.cpp
+++ tests/breakup.cpp
@@ -2,8 +2,10 @@
 #include "../src/PBESystem/breakupKernels/binaryBreakup.H"
 
 TEST(BreakupKernel, source){
-    Foam::dimensionedScalar xi("a_class", Foam::dimensionSet(0, 3, 0, 0, 0), 6);
-    Foam::dimensionedScalar result("result", Foam::dimensionSet(0, 3, 0, 0, 0), 36);
+    Foam::dimensionedScalar xi(
+        "a_class", Foam::dimensionSet(0, 3, 0, 0, 0), 6);
+    Foam::dimensionedScalar result(
+        "result", Foam::dimensionSet(0, 3, 0, 0, 0), 36);
     Foam::breakupKernels::binaryBreakup kernel;
     ASSERT_EQ(result.value(), kernel.S(xi).value());
 }
