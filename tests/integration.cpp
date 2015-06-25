178bdfc5e9dab02d0d91c19e5f86509a07ac5b8e
Kojirion <kojirion@gmail.com>
2015-06-25 15:50:20 +0100

    Add lower_bound parameter to integrate()

--- tests/integration.cpp
+++ tests/integration.cpp
@@ -7,7 +7,7 @@ TEST(Integration, exponential_inverse){
 
     auto f = [](double x){return std::exp(-x);};
 
-    auto r = integrate(f);
+    auto r = integrate(f, 0.);
 
     ASSERT_DOUBLE_EQ(r, 1.);
 }
