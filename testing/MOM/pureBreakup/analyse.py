777d5cdbcf31e882aa97f745ca9ff136b78ca527
Robert Sawko <r.sawko@cranfield.ac.uk>
2015-07-08 09:26:07 +0100

    Add visualisation to MOM vs MOC test

--- testing/MOM/pureBreakup/analyse.py
+++ testing/MOM/pureBreakup/analyse.py
@@ -1,23 +1,29 @@
-from numpy import genfromtxt, array, dot
-from os import listdir
-import re
+from numpy import genfromtxt, array, dot, column_stack, vstack, append
 from setupIC import class_number, v, dv, m0, prob
 import matplotlib as mpl
 mpl.use("agg")
 import matplotlib.pyplot as plt
 
 path = "./MOC{0}/postProcessing/probes/0/".format(class_number)
-classes = listdir(path)
 
-data = dict(
-    (
-        int(re.findall('\d+', c)[0]),
-        genfromtxt(path + c)[:, 1]
-    ) for c in classes
-)
+MOCdata = genfromtxt(path + 'n0')
+
+for k in range(1, class_number):
+    MOCdata = column_stack(
+        (MOCdata, genfromtxt(path + 'n{0}'.format(k))[:, 1])
+    )
+
+path = "./postProcessing/probes/0/"
+
+MOMdata = genfromtxt(path + 'm0', usecols=1)
+for k in range(1, 3):
+    MOMdata = column_stack(
+        (MOMdata, genfromtxt(path + 'm{0}'.format(k))[:, 1])
+    )
 
 NDFinit = m0 * (prob.cdf(v) - prob.cdf(v - dv)) / dv
-NDFend = array([data[i][-1] for i in range(class_number)]) / dv
+NDFend = MOCdata[-1][1:] / dv
+time = append([0.0], MOCdata[:, 0])
 
 
 def calculate_moment(N, k=0):
@@ -31,7 +37,22 @@ ax.plot(v, NDFinit)
 ax.plot(v, NDFend)
 fig.savefig("pbe.pdf", bbox_inches='tight')
 
+MOC_moments = array([calculate_moment(NDFinit, k) for k in range(3)])
+for n in range(MOCdata.shape[0]):
+    ndf = MOCdata[n][1:] / dv
+    MOC_moments = vstack(
+        (
+            MOC_moments,
+            array([calculate_moment(ndf, k) for k in range(3)])
+        )
+    )
+
+
 for k in range(3):
-    print(k)
-    print(calculate_moment(NDFinit, k))
-    print(calculate_moment(NDFend, k))
+    fig = plt.figure()
+    ax = fig.gca()
+    plt.xlabel('Time [s]')
+    plt.ylabel('Moment {0}'.format(k))
+    ax.plot(time, MOC_moments[:, k])
+    ax.plot(time[1:], MOMdata[:, k])
+    fig.savefig('moment{0}.pdf'.format(k), bbox_inches='tight')
