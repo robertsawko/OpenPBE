334ae723521930c7c197fb0ef32738b463ed0938
Kojirion <kojirion@gmail.com>
2015-07-16 12:38:19 +0100

    Get QMOM to compile

--- src/PBESystem/QMOM.C
+++ src/PBESystem/QMOM.C
@@ -118,80 +118,7 @@ QMOM::QMOM
             IOobject::AUTO_WRITE
         ),
         pow(6.0 / pi * moments_[1] / moments_[0], 1.0 / 3.0)
-    ) ,
-    gamma_k_
-    (
-        IOobject
-        (
-            "gamma_k",
-            mesh_.time().timeName(),
-            mesh_,
-            IOobject::NO_READ,
-            IOobject::AUTO_WRITE
-        ),
-        mesh_,
-        dimensionedScalar("gamma_k", dimless, 0.0)
-    ),
-    gamma_theta_
-    (
-        IOobject
-        (
-            "gamma_theta",
-            mesh_.time().timeName(),
-            mesh_,
-            IOobject::NO_READ,
-            IOobject::AUTO_WRITE
-        ),
-        mesh_,
-        dimensionedScalar("gamma_theta", dimless / dimVolume, 0.0)
-    ),
-    gamma_c0_
-    (
-        IOobject
-        (
-            "gamma_c0",
-            mesh_.time().timeName(),
-            mesh_,
-            IOobject::NO_READ,
-            IOobject::AUTO_WRITE
-        ),
-        mesh_,
-        dimensionedScalar("c0", dimless, 0.0)
-    ),
-    Nf_(QMOMDict_.lookupOrDefault<scalar>("daughterDropletsNr",2.0)),
-    maxD_
-    (
-        dimensionedScalar
-        (
-            "maxDiameter",
-            dimLength,
-            QMOMDict_.lookup("maxDiameter")
-        )
-    ),
-    minD_
-    (
-        dimensionedScalar
-        (
-            "minDiameter",
-            dimLength,
-            QMOMDict_.lookup("minDiameter")
-        )
-    ),
-    minGammaAlpha_
-    (
-        "minGammaAlpha",
-        dimLength,
-        QMOMDict_.lookupOrDefault<scalar>("minGammaAlpha", 1e-05)
-    ),
-    maxGammaBeta_(QMOMDict_.lookupOrDefault<scalar>("maxGammaBeta", 10.0)),
-    maxDiameterMultiplicator_
-    (
-        QMOMDict_.lookupOrDefault<scalar>("maxDiameterMultiplicator", 10.0)
-    ),
-    integrationSteps_(QMOMDict_.lookupOrDefault<scalar>("integrationSteps", 10)),
-    bList_(integrationSteps_),
-    cList_(pow(integrationSteps_, 2)),
-    gammaList_(integrationSteps_)
+    )
 {
 }
 
@@ -201,72 +128,24 @@ QMOM::~QMOM()
 }
 void QMOM::correct()
 {
-    Info<< "updating size moments" << endl;
-    gamma_c0_ = moments_[0];
-    gamma_k_ = pow(moments_[1] , 2)
-        / (moments_[2] * moments_[0] - pow(moments_[1],2)
-           + dimensionedScalar("small", dimensionSet(0,6,0,0,0), SMALL));
+    std::vector<volScalarField> mSources_;
 
-    gamma_theta_ = (moments_[0] * moments_[1]) 
-        / max(
-            moments_[0] * moments_[2] - pow(moments_[1], 2), 
-            dimensionedScalar("small", dimensionSet(0,6,0,0,0), SMALL)
-        );
-    
-    
-    Info<< "gamma parameters:" << endl;
-    printAvgMaxMin(gamma_c0_);
-    printAvgMaxMin(gamma_k_);
-    printAvgMaxMin(gamma_theta_);
-
-    //TODO: get rid of 3
-    std::array<volScalarField, 3> mSources_{{
-        volScalarField
-        (
-            IOobject
-            (
-                "m0Source",
-                mesh_.time().timeName(),
-                mesh_,
-                IOobject::NO_READ,
-                IOobject::AUTO_WRITE
-            ),
-            momentSourceTerm(0)
-            //dispersedPhase_.U().mesh(),
-            //dimensionedScalar("m3Source", dimVolume / dimTime, 0.0)
-        ),
-        volScalarField
-        (
-            IOobject
-            (
-                "m1Source",
-                mesh_.time().timeName(),
-                mesh_,
-                IOobject::NO_READ,
-                IOobject::AUTO_WRITE
-            ),
-            momentSourceTerm(1)
-        ),
-        volScalarField
-        (
-            IOobject
-            (
-                "m2Source",
-                mesh_.time().timeName(),
-                mesh_,
-                IOobject::NO_READ,
-                IOobject::AUTO_WRITE
-            ),
-            momentSourceTerm(2)
-        )
-    }};
+    for (std::size_t i = 0; i<moments_.size(); ++i){
+        mSources_.emplace_back
+                (
+                    IOobject
+                    (
+                        "m" + std::to_string(i) + "Source",
+                        mesh_.time().timeName(),
+                        mesh_,
+                        IOobject::NO_READ,
+                        IOobject::AUTO_WRITE
+                    ),
+                    momentSourceTerm(i)
+                );
+    }
 
     Info<< "moment sources:" << endl;
-    //TODO: is there a better way to assure that source terms on boundaries
-    //are equal to 0?
-    //maybe using internalField() instead of the whole field somewhere?
-    //how source terms are handled in combustion/chemistry/turbulence codes in
-    //OF
     for (auto& mSource : mSources_)
     {
         mSource.boundaryField() = 0;
@@ -290,7 +169,6 @@ void QMOM::correct()
         (
             fvm::ddt(moments_[i])
             + fvm::div(dispersedPhase_.phi(),moments_[i])
-            //+ fvm::div(limitedFlux_,moments_[i])
             ==
             mSources_[i]
         );
@@ -309,8 +187,8 @@ void QMOM::correct()
     }
 
     d_ = pow(6.0 / pi * moments_[1] / moments_[0], 1.0/3.0);
-    d_ = min(d_, maxD_);
-    d_ = max(d_,  minD_);
+    //clamp d_ here?
+
 
     printAvgMaxMin(d_);
 }
@@ -336,18 +214,6 @@ tmp<volScalarField> QMOM::momentSourceTerm(label momenti)
 
 tmp<volScalarField> QMOM::coalescenceSourceTerm(label momenti)
 {
-    dimensionedScalar xiDim = dimensionedScalar("xiDim", dimLength, 1.0);
-
-    scalar dMean = max(d_.weightedAverage(mesh_.V()).value(), SMALL);
-    scalar maxArg = maxDiameterMultiplicator_ * dMean ;
-    label maxIter = integrationSteps_;
-    dimensionedScalar dx = xiDim * maxArg / integrationSteps_;
-
-    dimensionedScalar arg_i1 = xiDim;
-    dimensionedScalar arg_j1 = xiDim;
-    label iterator = 0;
-    label iterator2 = 0;
-
     //value of the integral
     volScalarField sum
         (
@@ -430,27 +296,27 @@ tmp<volScalarField> QMOM::breakupSourceTerm(label momenti)
             ) 
         );
 
-    forAll(dispersedPhase_, celli)
-    {
-        gammaDistribution gamma(gamma_k_[celli], gamma_theta_[celli]);
+//    forAll(dispersedPhase_, celli)
+//    {
+//        gammaDistribution gamma(gamma_k_[celli], gamma_theta_[celli]);
 
-        auto m0 = moments_[0][celli];
+//        auto m0 = moments_[0][celli];
 
-        auto breakupSourceIntegrand = [&](double xi){
-            scalar breakupDeath = breakup_->S(xi).value() * m0 * pdf(gamma, xi);
+//        auto breakupSourceIntegrand = [&](double xi){
+//            scalar breakupDeath = breakup_->S(xi).value() * m0 * pdf(gamma, xi);
 
-            auto breakupBirthIntegrand = [&, xi](double xi_prime){
-                return daughterParticleDistribution_->beta(xi, xi_prime).value()
-                    * breakup_->S(xi_prime).value() * m0 * pdf(gamma, xi_prime);
-            };
+//            auto breakupBirthIntegrand = [&, xi](double xi_prime){
+//                return daughterParticleDistribution_->beta(xi, xi_prime).value()
+//                    * breakup_->S(xi_prime).value() * m0 * pdf(gamma, xi_prime);
+//            };
 
-            double breakupBirth = integrate(breakupBirthIntegrand, xi);
+//            double breakupBirth = integrate(breakupBirthIntegrand, xi);
 
-            return pow(xi, momenti) * (breakupBirth - breakupDeath);
-        };
+//            return pow(xi, momenti) * (breakupBirth - breakupDeath);
+//        };
         
-        toReturn[celli] = integrate(breakupSourceIntegrand, 0.);
-    }
+//        toReturn[celli] = integrate(breakupSourceIntegrand, 0.);
+//    }
 
     return tmp<volScalarField>( new volScalarField(toReturn));
 }
