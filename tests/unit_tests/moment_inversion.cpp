daa4ce5a93ca9d18f93e75a49608f792a2090c48
Robert Sawko <r.sawko@cranfield.ac.uk>
2015-07-16 20:48:02 +0100

    Add second example for Wheeler unit test

--- tests/unit_tests/moment_inversion.cpp
+++ tests/unit_tests/moment_inversion.cpp
@@ -12,7 +12,7 @@ Particulate and Multiphase Systems", Cambridge University Press 2013
 
 using Eigen::VectorXd;
 
-TEST(qmom, wheeler_test){
+TEST(wheeler_inversion, example31){
 
     VectorXd moments(8);
     moments << 1, 5, 26, 140, 778, 4450, 26140, 157400;
@@ -23,8 +23,27 @@ TEST(qmom, wheeler_test){
     weights <<  w1, w2, w2, w1;
     abcissas << 2.6656, 4.2580, 5.7420, 7.3344;
 
-    auto p = wheeler_inversion(moments);
+    auto result = wheeler_inversion(moments);
     
-    ASSERT_NEAR((p.abcissas - abcissas).norm(), 0, 1e-4);
-    ASSERT_NEAR((p.weights - weights).norm(), 0, 1e-4);
+    ASSERT_NEAR((result.abcissas - abcissas).norm(), 0, 1e-4);
+    ASSERT_NEAR((result.weights - weights).norm(), 0, 1e-4);
+}
+
+TEST(wheeler_inversion, example32){
+
+    VectorXd moments(8);
+    moments << 1, 0, 1, 0, 3, 0, 15, 0;
+    double xi1 = sqrt(sqrt(6) + 3);
+    double xi2 = sqrt(3 - sqrt(6));
+    // Analytical solution
+    double w1 = 1 / (2 * sqrt(6) + pow(2, 1.5) * sqrt(3) + 12); 
+    double w2 = 1 / (12 - 2 * sqrt(6) - pow(2, 1.5) * sqrt(3)); 
+    VectorXd weights(4), abcissas(4);
+    weights <<  w1, w2, w2, w1;
+    abcissas << -xi1, -xi2, xi2, xi1;
+
+    auto result = wheeler_inversion(moments);
+    
+    ASSERT_NEAR((result.abcissas - abcissas).norm(), 0, 1e-4);
+    ASSERT_NEAR((result.weights - weights).norm(), 0, 1e-4);
 }
