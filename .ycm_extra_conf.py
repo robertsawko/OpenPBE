5ec94bb9f94e88ddb9fdb11cfd94f7628de179d9
Robert Sawko <r.sawko@cranfield.ac.uk>
2015-07-17 09:32:35 +0100

    Correct ycm config to account for gtest include

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -76,7 +76,7 @@ flags = [
     '-I./src/PBESystem/',
     '-I.',
     '-I' + foam_src + '/OSspecific/POSIX/lnInclude',
-    '-I./build/ext/gtest/src/googletest/include/gtest'
+    '-I./build/ext/gtest/src/googletest/include/'
 ]
 
 # Set this to the absolute path to the folder (NOT the file!) containing the
@@ -94,36 +94,36 @@ else:
 
 
 def DirectoryOfThisScript():
-  return os.path.dirname( os.path.abspath( __file__ ) )
+    return os.path.dirname(os.path.abspath(__file__))
 
 
-def MakeRelativePathsInFlagsAbsolute( flags, working_directory ):
-  if not working_directory:
-    return list( flags )
-  new_flags = []
-  make_next_absolute = False
-  path_flags = [ '-isystem', '-I', '-iquote', '--sysroot=' ]
-  for flag in flags:
-    new_flag = flag
+def MakeRelativePathsInFlagsAbsolute(flags, working_directory):
+    if not working_directory:
+        return list(flags)
+    new_flags = []
+    make_next_absolute = False
+    path_flags = ['-isystem', '-I', '-iquote', '--sysroot=']
+    for flag in flags:
+        new_flag = flag
 
-    if make_next_absolute:
-      make_next_absolute = False
-      if not flag.startswith( '/' ):
-        new_flag = os.path.join( working_directory, flag )
+        if make_next_absolute:
+            make_next_absolute = False
+            if not flag.startswith('/'):
+                new_flag = os.path.join(working_directory, flag)
 
-    for path_flag in path_flags:
-      if flag == path_flag:
-        make_next_absolute = True
-        break
+        for path_flag in path_flags:
+            if flag == path_flag:
+                make_next_absolute = True
+                break
 
-      if flag.startswith( path_flag ):
-        path = flag[ len( path_flag ): ]
-        new_flag = path_flag + os.path.join( working_directory, path )
-        break
+            if flag.startswith(path_flag):
+                path = flag[len(path_flag):]
+                new_flag = path_flag + os.path.join(working_directory, path)
+                break
 
-    if new_flag:
-      new_flags.append( new_flag )
-  return new_flags
+        if new_flag:
+            new_flags.append(new_flag)
+    return new_flags
 
 
 def FlagsForFile( filename ):
