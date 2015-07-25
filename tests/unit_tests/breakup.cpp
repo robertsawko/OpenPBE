cf0aba3cc8e8813835a66d6cd517d114a54dbee4
Kojirion <kojirion@gmail.com>
2015-07-25 14:22:46 +0100

    Unit test the breakupKernel.S() dimensions as well

--- tests/unit_tests/breakup.cpp
+++ tests/unit_tests/breakup.cpp
@@ -1,11 +1,17 @@
 #include <gtest/gtest.h>
 #include "breakupKernels/binaryBreakup.H"
 
-TEST(GSL_integrator, source){
+TEST(breakupKernel, source){
     Foam::dimensionedScalar xi(
-        "a_class", Foam::dimensionSet(0, 3, 0, 0, 0), 6);
-    Foam::dimensionedScalar result(
-        "result", Foam::dimensionSet(0, 3, 0, 0, 0), 36);
+        "xi", Foam::dimensionSet(0, 3, 0, 0, 0), 6);
+
+    Foam::dimensionedScalar expected(
+        "expected", Foam::dimensionSet(0, 0, -1, 0, 0), 36);
+
     Foam::breakupKernels::binaryBreakup kernel;
-    ASSERT_EQ(result.value(), kernel.S(xi).value());
+
+    auto result = kernel.S(xi);
+
+    ASSERT_EQ(expected.value(), result.value());
+    ASSERT_EQ(expected.dimensions(), result.dimensions());
 }
