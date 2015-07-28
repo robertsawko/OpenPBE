cbce0bd93c439f1ba25da5419f99c7102312d36a
Robert Sawko <r.sawko@cranfield.ac.uk>
2015-07-28 18:23:51 +0100

    Add preliminary template for QMOM tests

---
+++ tests/integration/QMOM/setupCases.py
@@ -0,0 +1,104 @@
+from os import path
+from numpy import exp, arange, array
+from PyFoam.RunDictionary.SolutionDirectory import SolutionDirectory
+from PyFoam.RunDictionary.ParsedParameterFile import ParsedParameterFile
+
+
+class test_case:
+    def __init__(
+        self, case_name, nr_classes, vmax,
+        Ninit, phase_properties_name,
+        end_time, delta_t
+    ):
+        self.name = case_name
+        self.nr_classes = nr_classes
+        self.vmax = vmax
+        self.dv = float(vmax) / nr_classes
+        self.Ninit = Ninit
+        self.phase_properties_name = phase_properties_name
+        self.end_time = end_time
+        self.delta_t = delta_t
+
+
+class breakup_case(test_case):
+
+    def __init__(self, nr_classes):
+        self.l = 1.0
+
+        def Nbr0(v):
+            if(v < self.l):
+                return 0
+            else:
+                return 1
+
+        test_case.__init__(
+            self, "pure_br", nr_classes, self.l,
+            Nbr0,
+            "phaseProperties.breakup",
+            10, 0.005
+
+        )
+
+
+class coalescence_case(test_case):
+
+    def __init__(self, nr_classes, N0=2, v0=0.5, C=0.1):
+        vmax = 1e1
+        dv = float(vmax) / nr_classes
+        self.N0 = N0
+        self.v0 = v0
+        self.C = C
+        test_case.__init__(
+            self, "pure_coal", nr_classes, vmax,
+            lambda v: (N0 / v0) * (v / v0) * exp(-v / v0) * dv,
+            "phaseProperties.coalescence",
+            1, 0.001
+        )
+
+
+def case_setup(ci):
+    template_case = SolutionDirectory(
+        "template", archive=None, paraviewLink=False)
+    case = template_case.cloneCase(
+        "{0}{1}".format(ci.name, ci.nr_classes)
+    )
+
+    phase_properties = ParsedParameterFile(
+        path.join("./diffs", ci.phase_properties_name))
+    phase_properties["air"]["PBEDiameterCoeffs"]["QMOMCoeffs"]["numberOfClasses"] = ci.nr_classes
+
+    # manually fix bad pyfoam parsing
+    phase_properties["blending"]["default"]["type"] = "none"
+    phase_properties["drag"][1]["swarmCorrection"]["type"] = "none"
+    phase_properties.writeFileAs(path.join(
+        case.name, "constant", "phaseProperties"
+    ))
+
+    v = ci.dv + ci.dv * arange(ci.nr_classes)
+    m0 = ParsedParameterFile(path.join(template_case.name, "0", "m0"))
+    for i in range(ci.nr_classes):
+        m0.header["object"] = "m" + str(i)
+        m0["internalField"].setUniform(ci.Ninit(v[i]))
+        m0["dimensions"] = "[0 {0} 0 0 0 0 0]".format(3 * i)
+        m0.writeFileAs(path.join(case.name, "0", "m" + str(i)))
+
+    controlDict = ParsedParameterFile(
+        path.join(case.name, "system", "controlDict")
+    )
+    controlDict["functions"]["probes"]["fields"] = [
+        "n{0}".format(n) for n in range(ci.nr_classes)]
+    controlDict["endTime"] = ci.end_time
+    controlDict["deltaT"] = ci.delta_t
+    controlDict.writeFile()
+
+pbe_grids = [10, 20, 40, 80]
+coalescence_cases = [coalescence_case(nC) for nC in pbe_grids]
+breakup_cases = [breakup_case(nC) for nC in pbe_grids]
+
+if __name__ == "__main__":
+
+    for bc in breakup_cases:
+        case_setup(bc)
+
+    for cc in coalescence_cases:
+        case_setup(cc)
