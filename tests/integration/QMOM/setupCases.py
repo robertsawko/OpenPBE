f1397d7d4148838e790e004baa2829c2235221a5
Robert Sawko <r.sawko@cranfield.ac.uk>
2015-07-29 10:59:36 +0100

    Add moment evaluation for initial guesses

--- tests/integration/QMOM/setupCases.py
+++ tests/integration/QMOM/setupCases.py
@@ -1,20 +1,18 @@
 from os import path
-from numpy import exp, arange, array
+from numpy import ones, array
 from PyFoam.RunDictionary.SolutionDirectory import SolutionDirectory
 from PyFoam.RunDictionary.ParsedParameterFile import ParsedParameterFile
 
 
 class test_case:
     def __init__(
-        self, case_name, nr_classes, vmax,
-        Ninit, phase_properties_name,
+        self, case_name, quadrature_order,
+        initial_moments, phase_properties_name,
         end_time, delta_t
     ):
         self.name = case_name
-        self.nr_classes = nr_classes
-        self.vmax = vmax
-        self.dv = float(vmax) / nr_classes
-        self.Ninit = Ninit
+        self.quadrature_order = quadrature_order
+        self.initial_moments = initial_moments
         self.phase_properties_name = phase_properties_name
         self.end_time = end_time
         self.delta_t = delta_t
@@ -22,35 +20,33 @@ class test_case:
 
 class breakup_case(test_case):
 
-    def __init__(self, nr_classes):
+    def __init__(self, quadrature_order):
         self.l = 1.0
-
-        def Nbr0(v):
-            if(v < self.l):
-                return 0
-            else:
-                return 1
-
         test_case.__init__(
-            self, "pure_br", nr_classes, self.l,
-            Nbr0,
-            "phaseProperties.breakup",
-            10, 0.005
-
+            self, "pure_br", quadrature_order,
+            initial_moments=ones(2 * quadrature_order),
+            phase_properties_name="phaseProperties.breakup",
+            end_time=10, delta_t=0.005
         )
 
 
 class coalescence_case(test_case):
 
-    def __init__(self, nr_classes, N0=2, v0=0.5, C=0.1):
-        vmax = 1e1
-        dv = float(vmax) / nr_classes
+    def __init__(self, quadrature_order, N0=2, v0=0.5, C=0.1):
         self.N0 = N0
         self.v0 = v0
         self.C = C
+
+        from sympy import simplify, symbols, integrate, exp, oo
+        v = symbols('v', real=True, positive=True)
+
+        initial_moments = N0 / v0**2 * array([
+            float(simplify(integrate(v**(k + 1) * exp(-v / v0), (v, 0, oo))))
+            for k in range(2 * quadrature_order)])
+
         test_case.__init__(
-            self, "pure_coal", nr_classes, vmax,
-            lambda v: (N0 / v0) * (v / v0) * exp(-v / v0) * dv,
+            self, "pure_coal", quadrature_order,
+            initial_moments,
             "phaseProperties.coalescence",
             1, 0.001
         )
@@ -60,12 +56,12 @@ def case_setup(ci):
     template_case = SolutionDirectory(
         "template", archive=None, paraviewLink=False)
     case = template_case.cloneCase(
-        "{0}{1}".format(ci.name, ci.nr_classes)
+        "{0}{1}".format(ci.name, ci.quadrature_order)
     )
 
     phase_properties = ParsedParameterFile(
         path.join("./diffs", ci.phase_properties_name))
-    phase_properties["air"]["PBEDiameterCoeffs"]["QMOMCoeffs"]["numberOfClasses"] = ci.nr_classes
+    phase_properties["air"]["PBEDiameterCoeffs"]["QMOMCoeffs"]["numberOfClasses"] = ci.quadrature_order
 
     # manually fix bad pyfoam parsing
     phase_properties["blending"]["default"]["type"] = "none"
@@ -74,11 +70,10 @@ def case_setup(ci):
         case.name, "constant", "phaseProperties"
     ))
 
-    v = ci.dv + ci.dv * arange(ci.nr_classes)
     m0 = ParsedParameterFile(path.join(template_case.name, "0", "m0"))
-    for i in range(ci.nr_classes):
+    for i in range(2 * ci.quadrature_order):
         m0.header["object"] = "m" + str(i)
-        m0["internalField"].setUniform(ci.Ninit(v[i]))
+        m0["internalField"].setUniform(ci.initial_moments[i])
         m0["dimensions"] = "[0 {0} 0 0 0 0 0]".format(3 * i)
         m0.writeFileAs(path.join(case.name, "0", "m" + str(i)))
 
@@ -86,12 +81,12 @@ def case_setup(ci):
         path.join(case.name, "system", "controlDict")
     )
     controlDict["functions"]["probes"]["fields"] = [
-        "n{0}".format(n) for n in range(ci.nr_classes)]
+        "m{0}".format(m) for m in range(ci.quadrature_order)]
     controlDict["endTime"] = ci.end_time
     controlDict["deltaT"] = ci.delta_t
     controlDict.writeFile()
 
-pbe_grids = [10, 20, 40, 80]
+pbe_grids = [1, 2, 4, 6]
 coalescence_cases = [coalescence_case(nC) for nC in pbe_grids]
 breakup_cases = [breakup_case(nC) for nC in pbe_grids]
 
