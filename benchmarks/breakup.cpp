fcb28170c2c2140c178b671bb36efd41b357f2b2
Robert Sawko <robertsawko@gmail.com>
2015-12-28 01:40:46 +0100

    Bring back benchmarks

--- benchmarks/breakup.cpp
+++ benchmarks/breakup.cpp
@@ -31,12 +31,8 @@ static void CoulaloglouTavlaridesBreakup(benchmark::State& state) {
     Foam::scalar C1(0.0152), C2(0.0678),
                  sigma(0.04282), alpha(0.050000000000000003);
 
-    Foam::dimensionedScalar epsilon("epsilon", Foam::dimless,
-                                    0.12935361098784559);
-    Foam::dimensionedScalar rho_d("rho_d", Foam::dimless, 972.0);
-
-    Foam::dimensionedScalar xi(
-        "xi", Foam::dimensionSet(0, 3, 0, 0, 0), 9.738310380055584507e-11);
+    Foam::scalar epsilon(0.12935361098784559), rho_d(972.0),
+           xi(9.738310380055584507e-11);
 
     Foam::breakupKernels::CoulaloglouTavlaridesImp kernel(C1, C2, alpha, sigma);
 
--- benchmarks/coalescence.cpp
+++ benchmarks/coalescence.cpp
@@ -4,33 +4,22 @@
 #include "coalescenceKernels/CoulaloglouTavlarides.H"
 
 static void constantCoalescence(benchmark::State& state) {
-    Foam::dimensionedScalar xi1(
-        "xi1", Foam::dimensionSet(0, 3, 0, 0, 0), 6);
-
-    Foam::dimensionedScalar xi2(
-        "xi2", Foam::dimensionSet(0, 3, 0, 0, 0), 8);
-
     Foam::scalar c = 5;
 
     Foam::coalescenceKernels::constantCoalescenceImpl kernel(c);
 
     while (state.KeepRunning())
-        kernel.S(xi1, xi2);
+        kernel.S();
 }
 
 BENCHMARK(constantCoalescence);
 
 static void noCoalescence(benchmark::State& state) {
-    Foam::dimensionedScalar xi1(
-        "xi1", Foam::dimensionSet(0, 3, 0, 0, 0), 6);
-
-    Foam::dimensionedScalar xi2(
-        "xi2", Foam::dimensionSet(0, 3, 0, 0, 0), 8);
 
     Foam::coalescenceKernels::noCoalescenceImpl kernel;
 
     while (state.KeepRunning())
-        kernel.S(xi1, xi2);
+        kernel.S();
 }
 
 BENCHMARK(noCoalescence);
@@ -39,20 +28,9 @@ static void CoulaloglouTavlaridesCoalescence(benchmark::State& state) {
     Foam::scalar C1(0.0152), C2(0.0678),
                  sigma(0.04282), alpha(0.050000000000000003);
 
-    Foam::dimensionedScalar epsilon("epsilon", Foam::dimless,
-                                    0.12935361098784559);
-    Foam::dimensionedScalar rho_d("rho_d", Foam::dimless, 972.0);
-
-    Foam::dimensionedScalar nud("nu_d", Foam::dimless, 0.001); //actually muc
-
-    Foam::dimensionedScalar xi1(
-        "xi1", Foam::dimensionSet(0, 3, 0, 0, 0), 5.071423503403169858e-10);
-
-    Foam::dimensionedScalar xi2(
-        "xi2", Foam::dimensionSet(0, 3, 0, 0, 0), 2.885425297794245861e-11);
-
-    Foam::dimensionedScalar Vcell(
-        "xi2", Foam::dimensionSet(0, 3, 0, 0, 0), 0.012);
+    Foam::scalar epsilon(0.12935361098784559), rho_d(972.0), nud(0.001),
+        xi1(5.071423503403169858e-10), xi2(2.885425297794245861e-11),
+        Vcell(0.012);
 
     Foam::coalescenceKernels::CoulaloglouTavlaridesCImpl kernel(
                 C1, C2, alpha, sigma);
