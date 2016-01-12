10f6069693f39b4a738046e9aa060793efe9616e
Kojirion <kojirion@gmail.com>
2016-01-12 01:43:40 +0000

    Add CT coalescence python binding

--- src/PBESystem/bindings/kernels.cpp
+++ src/PBESystem/bindings/kernels.cpp
@@ -1,12 +1,19 @@
 #include <boost/python.hpp>
 #include "../breakupKernels/CoulaloglouTavlarides.H"
+#include "../coalescenceKernels/CoulaloglouTavlarides.H"
 
 BOOST_PYTHON_MODULE(kernels)
 {
     using namespace boost::python;
     using Foam::scalar;
 
-    class_<Foam::breakupKernels::CoulaloglouTavlaridesImp>("CoulaloglouTavlaridesBreakup"
-                                     , init<scalar, scalar, scalar, scalar>())
+    class_<Foam::breakupKernels::CoulaloglouTavlaridesImp>(
+                "CoulaloglouTavlaridesBreakup",
+                init<scalar, scalar, scalar, scalar>())
             .def("S", &Foam::breakupKernels::CoulaloglouTavlaridesImp::S);
+
+    class_<Foam::coalescenceKernels::CoulaloglouTavlaridesCImpl>(
+                "CoulaloglouTavlaridesCoalescence",
+                init<scalar, scalar, scalar, scalar>())
+            .def("S", &Foam::coalescenceKernels::CoulaloglouTavlaridesCImpl::S);
 }
