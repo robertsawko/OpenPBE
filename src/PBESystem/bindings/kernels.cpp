d4939b61a0705a7d2fde4fc7590cf529dc330e94
Kojirion <kojirion@gmail.com>
2016-01-12 02:07:58 +0000

    Add bindings for binary and no breakup, constant and no coalescence kernels

--- src/PBESystem/bindings/kernels.cpp
+++ src/PBESystem/bindings/kernels.cpp
@@ -1,6 +1,10 @@
 #include <boost/python.hpp>
 #include "../breakupKernels/CoulaloglouTavlarides.H"
+#include "../breakupKernels/binaryBreakup.H"
+#include "../breakupKernels/noBreakup.H"
 #include "../coalescenceKernels/CoulaloglouTavlarides.H"
+#include "../coalescenceKernels/constantCoalescence.H"
+#include "../coalescenceKernels/noCoalescence.H"
 
 BOOST_PYTHON_MODULE(kernels)
 {
@@ -12,8 +16,25 @@ BOOST_PYTHON_MODULE(kernels)
                 init<scalar, scalar, scalar, scalar>())
             .def("S", &Foam::breakupKernels::CoulaloglouTavlaridesImp::S);
 
+    class_<Foam::breakupKernels::binaryBreakupImpl>(
+                "BinaryBreakup")
+            .def("S", &Foam::breakupKernels::binaryBreakupImpl::S);
+
+    class_<Foam::breakupKernels::noBreakupImpl>(
+                "NoBreakup")
+            .def("S", &Foam::breakupKernels::noBreakupImpl::S);
+
     class_<Foam::coalescenceKernels::CoulaloglouTavlaridesCImpl>(
                 "CoulaloglouTavlaridesCoalescence",
                 init<scalar, scalar, scalar, scalar>())
             .def("S", &Foam::coalescenceKernels::CoulaloglouTavlaridesCImpl::S);
+
+    class_<Foam::coalescenceKernels::constantCoalescenceImpl>(
+                "ConstantCoalescence",
+                init<scalar>())
+            .def("S", &Foam::coalescenceKernels::constantCoalescenceImpl::S);
+
+    class_<Foam::coalescenceKernels::noCoalescenceImpl>(
+                "NoCoalescence")
+            .def("S", &Foam::coalescenceKernels::noCoalescenceImpl::S);
 }
