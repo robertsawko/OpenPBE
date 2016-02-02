716698f4f9563c186716e3251ffa3b48101efb32
Robert Sawko <r.sawko@cranfield.ac.uk>
2016-02-02 13:20:10 +0000

    Add precalculation of tmp fields to coalescence
    This fixes validity problem mentioned in #11.

--- src/PBESystem/MOC.C
+++ src/PBESystem/MOC.C
@@ -33,26 +33,21 @@ License
 #include "Utility.H"
 //#include <boost/timer/timer.hpp>
 
-
-
-namespace Foam
-{
-namespace PBEMethods
-{
+namespace Foam {
+namespace PBEMethods {
 defineTypeNameAndDebug(MOC, 0);
 addToRunTimeSelectionTable(PBEMethod, MOC, dictionary);
 // * * * * * * * * * * * * * * * Static Member Data  * * * * * * * * * * * * //
 
-
 // * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
-
+inline dimensionedScalar volumeToDiameter(const dimensionedScalar &v) {
+    return pow(6.0 / constant::mathematical::pi * v, 1.0 / 3.0);
+}
 
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
 
 MOC::MOC(const dictionary &pbeProperties, const phaseModel &phase)
-    : PBEMethod(pbeProperties, phase),
-      deltaXi_("deltaXi", dimVolume, 0.)
-{
+    : PBEMethod(pbeProperties, phase), deltaXi_("deltaXi", dimVolume, 0.) {
     auto MOCDict_ = pbeProperties.subDict("MOCCoeffs");
     auto numberOfClasses_ = readLabel(MOCDict_.lookup("numberOfClasses"));
 
@@ -61,8 +56,7 @@ MOC::MOC(const dictionary &pbeProperties, const phaseModel &phase)
     deltaXi_.value() = readScalar(MOCDict_.lookup("xi1"));
 
     usingMULES_ = MOCDict_.lookupOrDefault<Switch>("usingMULES", false);
-    breakupCache_.resize(numberOfClasses_*phase.size());
-
+    breakupCache_.resize(numberOfClasses_ * phase.size());
 
     Info << "Creating " << numberOfClasses_ << " class";
     // Taking pedantry one step too far!
@@ -74,184 +68,140 @@ MOC::MOC(const dictionary &pbeProperties, const phaseModel &phase)
     forAll(classNumberDensity_, i) {
         classNumberDensity_.set(
             i,
-            new volScalarField(
-                   IOobject(
-                       "n" + std::to_string(i),
-                       phase.U().mesh().time().timeName(),
-                       phase.U().mesh(),
-                       IOobject::MUST_READ,
-                       IOobject::AUTO_WRITE),
-                   phase.U().mesh()));
+            new volScalarField(IOobject("n" + std::to_string(i),
+                                        phase.U().mesh().time().timeName(),
+                                        phase.U().mesh(),
+                                        IOobject::MUST_READ,
+                                        IOobject::AUTO_WRITE),
+                               phase.U().mesh()));
         classVelocity_.set(i, phase.U());
-        xi_.emplace_back(
-            "xi" + std::to_string(i), deltaXi_ * (i + 1));
-        Info << i << " has volume " << xi_[i] << endl;
+        xi_.emplace_back("xi" + std::to_string(i), deltaXi_ * (i + 1));
+        Info << i << " has volume " << xi_[i].value() << " and diameter "
+             << volumeToDiameter(xi_[i]).value() << endl;
     }
 }
 
 // * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
 
-volScalarField MOC::classSourceTerm(label i)
-{
+volScalarField MOC::classSourceTerm(label i) {
+    coalescence_().update();
     return coalescenceSourceTerm(i) + breakupSourceTerm(i);
 }
-volScalarField MOC::coalescenceSourceTerm(label i)
-{
-    //boost::timer::auto_cpu_timer t;
-    volScalarField coalescenceField
-    (
-        IOobject
-        (
-            "Scoal",
-            classNumberDensity_[i].mesh().time().timeName(),
-            classNumberDensity_[i].mesh(),
-            IOobject::NO_READ,
-            IOobject::NO_WRITE,
-            false
-        ),
+volScalarField MOC::coalescenceSourceTerm(label i) {
+    // boost::timer::auto_cpu_timer t;
+    volScalarField coalescenceField(
+        IOobject("Scoal",
+                 classNumberDensity_[i].mesh().time().timeName(),
+                 classNumberDensity_[i].mesh(),
+                 IOobject::NO_READ,
+                 IOobject::NO_WRITE,
+                 false),
         classNumberDensity_[i].mesh(),
-        //TODO Is the dimension correct?
-        dimensionedScalar
-        (
-            "Scoal", 
-            classNumberDensity_[i].dimensions() / dimTime,
-            0
-        ) 
-    );
+        // TODO Is the dimension correct?
+        dimensionedScalar(
+            "Scoal", classNumberDensity_[i].dimensions() / dimTime, 0));
 
-    forAll(phase_, celli){
+    forAll(phase_, celli) {
         // the upper limit stops from removing mass through coalescence that
         // results in drops that are outside of PBE domain
-        for(int j = 0; j < classNumberDensity_.size() - i - 1; ++j){
-            coalescenceField[celli] -=
-                coalescence_().S(xi_[i], xi_[j], celli)
-                * classNumberDensity_[j][celli];
-        }
 
         coalescenceField[celli] *= classNumberDensity_[i][celli];
         //-1 in pairs account for zero-based numbering
-        for(int j = 0; j < i; ++j){
+        for (int j = 0; j < i; ++j) {
             coalescenceField[celli] +=
-                0.5 * coalescence_().S(xi_[i - j - 1], xi_[j], celli)
-                * classNumberDensity_[i - j - 1][celli]
-                * classNumberDensity_[j][celli];
+                0.5 * coalescence_().S(xi_[i - j - 1], xi_[j], celli) *
+                classNumberDensity_[i - j - 1][celli] *
+                classNumberDensity_[j][celli];
         }
     }
     return coalescenceField;
-
 }
 
-volScalarField MOC::breakupSourceTerm(label i)
-{
-    //boost::timer::auto_cpu_timer t;
+volScalarField MOC::breakupSourceTerm(label i) {
+    // boost::timer::auto_cpu_timer t;
     volScalarField breakupField(
-        IOobject
-        (
-            "Sbr",
-            classNumberDensity_[i].mesh().time().timeName(),
-            classNumberDensity_[i].mesh(),
-            IOobject::NO_READ,
-            IOobject::NO_WRITE,
-            false
-        ),
+        IOobject("Sbr",
+                 classNumberDensity_[i].mesh().time().timeName(),
+                 classNumberDensity_[i].mesh(),
+                 IOobject::NO_READ,
+                 IOobject::NO_WRITE,
+                 false),
         classNumberDensity_[i].mesh(),
-        //TODO Is the dimension correct?
-        dimensionedScalar
-        (
-            "Sbr", 
-            classNumberDensity_[i].dimensions() / dimTime,
-            0.0
-        ) 
-    );
+        // TODO Is the dimension correct?
+        dimensionedScalar(
+            "Sbr", classNumberDensity_[i].dimensions() / dimTime, 0.0));
 
     auto phaseSize = phase_.size();
 
-    forAll(phase_, celli){
-        breakupField[celli] -=
-            breakupCache_[i*phaseSize+celli] * classNumberDensity_[i][celli];
+    forAll(phase_, celli) {
+        breakupField[celli] -= breakupCache_[i * phaseSize + celli] *
+                               classNumberDensity_[i][celli];
 
-        for (label j = i + 1; j < classNumberDensity_.size(); ++j){
+        for (label j = i + 1; j < classNumberDensity_.size(); ++j) {
             // deltaXi comes from the application of mean value theorem on the
             // second integral see Kumar and Ramkrishna (1996) paper
             breakupField[celli] +=
-                daughterParticleDistribution_().beta(xi_[i].value(), xi_[j].value())
-                * breakupCache_[j*phaseSize+celli]
-                * classNumberDensity_[j][celli] * deltaXi_.value();
+                daughterParticleDistribution_().beta(xi_[i].value(),
+                                                     xi_[j].value()) *
+                breakupCache_[j * phaseSize + celli] *
+                classNumberDensity_[j][celli] * deltaXi_.value();
         }
     }
     return breakupField;
 }
 
-void MOC::correct(){
-    //boost::timer::auto_cpu_timer t;
+void MOC::correct() {
+    // boost::timer::auto_cpu_timer t;
 
     {
-    //boost::timer::auto_cpu_timer t;
+        // boost::timer::auto_cpu_timer t;
 
+        auto phaseSize = phase_.size();
 
-    auto phaseSize = phase_.size();
-
-    for (int i=0; i<classNumberDensity_.size(); ++i){
-        for (int j=0; j<phaseSize; ++j)
-            breakupCache_[i*phaseSize+j] = breakup_->S(xi_[i],j);
-    }
-
+        for (int i = 0; i < classNumberDensity_.size(); ++i) {
+            for (int j = 0; j < phaseSize; ++j)
+                breakupCache_[i * phaseSize + j] = breakup_->S(xi_[i], j);
+        }
     }
 
     PtrList<volScalarField> S(classNumberDensity_.size());
-    forAll(classNumberDensity_, k){
-        S.set(k, classSourceTerm(k));
-    }
+    forAll(classNumberDensity_, k) { S.set(k, classSourceTerm(k)); }
 
-    if(usingMULES_)
+    if (usingMULES_)
         solveWithMULES(S);
     else
         solveWithFVM(S);
 }
 
-void MOC::solveWithFVM(const PtrList<volScalarField>& S){
-    const fvMesh& mesh = classNumberDensity_[0].mesh();
+void MOC::solveWithFVM(const PtrList<volScalarField> &S) {
+    const fvMesh &mesh = classNumberDensity_[0].mesh();
 
-    forAll(classNumberDensity_, k){
-        surfaceScalarField phi = fvc::interpolate(classVelocity_[k]) & mesh.Sf();
-        fvScalarMatrix nEqn
-        (
-            fvm::ddt(classNumberDensity_[k])
-            + 
-            fvm::div(phi, classNumberDensity_[k], "div(U,nk)")
-            ==
-            S[k]
-        );
+    forAll(classNumberDensity_, k) {
+        surfaceScalarField phi =
+            fvc::interpolate(classVelocity_[k]) & mesh.Sf();
+        fvScalarMatrix nEqn(
+            fvm::ddt(classNumberDensity_[k]) +
+                fvm::div(phi, classNumberDensity_[k], "div(U,nk)") ==
+            S[k]);
         nEqn.relax();
         nEqn.solve();
     }
 }
 
-void MOC::solveWithMULES(const PtrList<volScalarField>& S){
-    const fvMesh& mesh = classNumberDensity_[0].mesh();
-    const surfaceScalarField& phi = phase_.phi();
+void MOC::solveWithMULES(const PtrList<volScalarField> &S) {
+    const fvMesh &mesh = classNumberDensity_[0].mesh();
+    const surfaceScalarField &phi = phase_.phi();
     PtrList<surfaceScalarField> phiNkCorrs(classNumberDensity_.size());
 
-    forAll(classNumberDensity_, k)
-    {
-        volScalarField& Nk = classNumberDensity_[k];
+    forAll(classNumberDensity_, k) {
+        volScalarField &Nk = classNumberDensity_[k];
 
-        phiNkCorrs.set
-        (
-            k,
-            new surfaceScalarField
-            (
-                "phi" + Nk.name() + "Corr",
-                fvc::flux
-                (
-                    phi,
-                    Nk,
-                    "div(phi," + Nk.name() + ')'
-                )
-            )
-        );
+        phiNkCorrs.set(k,
+                       new surfaceScalarField(
+                           "phi" + Nk.name() + "Corr",
+                           fvc::flux(phi, Nk, "div(phi," + Nk.name() + ')')));
 
-        surfaceScalarField& phiNkCorr = phiNkCorrs[k];
+        surfaceScalarField &phiNkCorr = phiNkCorrs[k];
 
         /*
         // Ensure that the flux at inflow BCs is preserved
@@ -277,50 +227,36 @@ void MOC::solveWithMULES(const PtrList<volScalarField>& S){
         */
         scalar maxNk = max(phase_ * mesh.V() / xi_[k]).value();
 
-        MULES::limit
-        (
-            1.0 / mesh.time().deltaT().value(),
-            geometricOneField(),
-            Nk,
-            phi,
-            phiNkCorr,
-            zeroField(), //implicit source?
-            S[k], 
-            maxNk,
-            0,
-            true
-        );
+        MULES::limit(1.0 / mesh.time().deltaT().value(),
+                     geometricOneField(),
+                     Nk,
+                     phi,
+                     phiNkCorr,
+                     zeroField(), // implicit source?
+                     S[k],
+                     maxNk,
+                     0,
+                     true);
     }
 
     MULES::limitSum(phiNkCorrs);
 
-    volScalarField volumeSum
-    (
-        IOobject
-        (
-            "sumVolumeNk",
-            mesh.time().timeName(),
-            mesh
-        ),
+    volScalarField volumeSum(
+        IOobject("sumVolumeNk", mesh.time().timeName(), mesh),
         mesh,
-        dimensionedScalar("sumVolumeNk", xi_[0].dimensions(), 0)
-    );
+        dimensionedScalar("sumVolumeNk", xi_[0].dimensions(), 0));
 
-    forAll(classNumberDensity_, k)
-    {
-        volScalarField& Nk = classNumberDensity_[k];
+    forAll(classNumberDensity_, k) {
+        volScalarField &Nk = classNumberDensity_[k];
 
-        surfaceScalarField& phiNk = phiNkCorrs[k];
+        surfaceScalarField &phiNk = phiNkCorrs[k];
         phiNk += upwind<scalar>(mesh, phi).flux(Nk);
 
-        MULES::explicitSolve
-        (
-            geometricOneField(),
-            Nk,
-            phiNk,
-            zeroField(), //implicit source?
-            S[k]
-        );
+        MULES::explicitSolve(geometricOneField(),
+                             Nk,
+                             phiNk,
+                             zeroField(), // implicit source?
+                             S[k]);
 
         volumeSum += xi_[k] * Nk;
     }
@@ -329,27 +265,20 @@ void MOC::solveWithMULES(const PtrList<volScalarField>& S){
     alphaPBE.internalField() = volumeSum.internalField() / mesh.V();
     volScalarField continuityError("continuityError", mag(alphaPBE - phase_));
 
-    Info<< "Max continuity error between advection and PBE: "
-        << max(continuityError).value() << endl;
-        
+    Info << "Max continuity error between advection and PBE: "
+         << max(continuityError).value() << endl;
 }
 
-inline dimensionedScalar volumeToDiameter(const dimensionedScalar& v)
-{
-    return pow(6.0 / constant::mathematical::pi * v, 1.0/3.0);
-}
-const volScalarField MOC::d() const
-{
+const volScalarField MOC::d() const {
     volScalarField sum = volumeToDiameter(xi_[0]) * classNumberDensity_[0];
-    volScalarField norm = classNumberDensity_[0];
-    for(int i = 1; i < classNumberDensity_.size(); ++i)
-    {
+    volScalarField norm = 1.0 * classNumberDensity_[0];
+    for (int i = 1; i < classNumberDensity_.size(); ++i) {
         sum += volumeToDiameter(xi_[i]) * classNumberDensity_[i];
         norm += classNumberDensity_[i];
     }
     return sum / norm;
 }
-} //end of PBEMethods namespace
-} //end of Foam namespace
+} // end of PBEMethods namespace
+} // end of Foam namespace
 
 // ************************************************************************* //
--- src/PBESystem/coalescenceKernels/CoulaloglouTavlarides.C
+++ src/PBESystem/coalescenceKernels/CoulaloglouTavlarides.C
@@ -26,43 +26,41 @@ License
 #include "CoulaloglouTavlarides.H"
 #include "addToRunTimeSelectionTable.H"
 
-namespace Foam
-{
-namespace coalescenceKernels
-{
+namespace Foam {
+namespace coalescenceKernels {
 defineTypeNameAndDebug(CoulaloglouTavlaridesC, 0);
-addToRunTimeSelectionTable
-(
-    coalescenceKernel,
-    CoulaloglouTavlaridesC,
-    dictionary
-);
+addToRunTimeSelectionTable(coalescenceKernel,
+                           CoulaloglouTavlaridesC,
+                           dictionary);
 
-CoulaloglouTavlaridesC::CoulaloglouTavlaridesC
-(const dictionary& coalescenceDict,
-    const phaseModel& dispersedPhase
-)
-:
-    coalescenceKernel(coalescenceDict, dispersedPhase),
-    impl_( coalescenceDict.lookupOrDefault<scalar>("c1",0.00487),
-           coalescenceDict.lookupOrDefault<scalar>("c2",0.008),
-           coalescenceDict.lookupOrDefault<scalar>("gamma",0.0),
-           coalescenceDict.lookupOrDefault<scalar>("sigma",0.047) ),
-    phase_(dispersedPhase),
-    rhod_(dispersedPhase.rho()),
-    nud_(dispersedPhase.nu()),
-    epsilonName_("epsilon." + phase_.name())
-{
+CoulaloglouTavlaridesC::CoulaloglouTavlaridesC(
+    const dictionary &coalescenceDict, const phaseModel &dispersedPhase)
+    : coalescenceKernel(coalescenceDict, dispersedPhase),
+      impl_(coalescenceDict.lookupOrDefault<scalar>("c1", 0.00487),
+            coalescenceDict.lookupOrDefault<scalar>("c2", 0.008),
+            coalescenceDict.lookupOrDefault<scalar>("gamma", 0.0),
+            coalescenceDict.lookupOrDefault<scalar>("sigma", 0.047)),
+      phase_(dispersedPhase),
+      rhod_(dispersedPhase.rho()),
+      nud_(dispersedPhase.nu()),
+      epsilonName_("epsilon." + phase_.name()) {}
+
+void CoulaloglouTavlaridesC::update(){
+    nud_ = dispersedPhase_.nu()();
 }
 
 scalar CoulaloglouTavlaridesC::S(const dimensionedScalar &xi1,
-                                            const dimensionedScalar &xi2,
-                                            label celli) const {
+                                 const dimensionedScalar &xi2,
+                                 label celli) const {
 
-    static const volScalarField& epsilonField (
+    static const volScalarField &epsilonField(
         phase_.U().mesh().lookupObject<volScalarField>(epsilonName_));
 
-    return impl_.S(xi1.value(), xi2.value(), epsilonField[celli], rhod_[celli], nud_[celli],
+    return impl_.S(xi1.value(),
+                   xi2.value(),
+                   epsilonField[celli],
+                   rhod_[celli],
+                   nud_[celli],
                    phase_.U().mesh().V()[celli]);
 }
 
@@ -70,47 +68,28 @@ CoulaloglouTavlaridesCImpl::CoulaloglouTavlaridesCImpl(scalar c1,
                                                        scalar c2,
                                                        scalar gamma,
                                                        scalar sigma)
-    :
-      c1_(c1), c2_(c2), gamma_(gamma), sigma_(sigma)
-{}
+    : c1_(c1), c2_(c2), gamma_(gamma), sigma_(sigma) {}
 
-scalar CoulaloglouTavlaridesCImpl::S(
-        scalar xi1,
-        scalar xi2,
-        scalar epsilon,
-        scalar rhoc,
-        scalar nuc,
-        scalar Vcell
-) const
-{
+scalar CoulaloglouTavlaridesCImpl::S(scalar xi1,
+                                     scalar xi2,
+                                     scalar epsilon,
+                                     scalar rhoc,
+                                     scalar nuc,
+                                     scalar Vcell) const {
     scalar xi1pow13 = pow(xi1, 1.0 / 3.0);
     scalar xi2pow13 = pow(xi2, 1.0 / 3.0);
 
-    scalar frequency =
-        c1_ * pow(epsilon, 1.0 / 3.0) *
-        pow(xi1pow13 + xi2pow13, 2) *
-        pow
-        (
-            pow(xi1, 2.0 / 9.0) + pow(xi2, 2.0 / 9.0),
-            0.5
-        )
-        / (1 + gamma_) / Vcell;
+    scalar frequency = c1_ * pow(epsilon, 1.0 / 3.0) *
+                       pow(xi1pow13 + xi2pow13, 2) *
+                       pow(pow(xi1, 2.0 / 9.0) + pow(xi2, 2.0 / 9.0), 0.5) /
+                       (1 + gamma_) / Vcell;
 
-
-    scalar rate =
-        exp
-        (
-            (- c2_ * rhoc * epsilon * (rhoc * nuc)
-            / ( pow(sigma_, 2) * pow(1 + gamma_, 3) )
-            * pow
-                (
-                    xi1pow13 * xi2pow13 / (xi1pow13 + xi2pow13),
-                    4.0
-                ))
-        );
+    scalar rate = exp((-c2_ * rhoc * epsilon * (rhoc * nuc) /
+                       (pow(sigma_, 2) * pow(1 + gamma_, 3)) *
+                       pow(xi1pow13 * xi2pow13 / (xi1pow13 + xi2pow13), 4.0)));
 
     return frequency * rate;
 }
 
-} //End namespace coalescenceKernels
-} //End namespace Foam
+} // End namespace coalescenceKernels
+} // End namespace Foam
