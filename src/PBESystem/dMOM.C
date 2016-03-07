6f22ea80106b55d8b9068ee2487b8282b67f716f
Robert Sawko <robertsawko@gmail.com>
2016-03-07 15:27:58 +0000

    Replace std::vector with PtrList
    There appears to be a problem with output when volScalarField is stored
    in std::vector. Only the last entry is written out with "AUTO_WRITE"
    mode. A temporary workaround is to return to OF native PtrList.

--- src/PBESystem/dMOM.C
+++ src/PBESystem/dMOM.C
@@ -68,26 +68,26 @@ dMOM::dMOM
     dMOMDict_(pbeProperties.subDict("dMOMCoeffs")),
     dispersedPhase_(phase),
     mesh_(dispersedPhase_.U().mesh()),
-    Sgammas_(),
-    breakupSource_(),
+    Sgammas_(NO_OF_MOMENTS),
+    breakupSource_(NO_OF_MOMENTS),
     log_d_bar_
     (
         IOobject
         (
-            "log(d bar)",
+            "logd_bar",
             mesh_.time().timeName(),
             mesh_,
             IOobject::NO_READ,
             IOobject::AUTO_WRITE
         ),
         mesh_,
-        dimensionedScalar("log(d bar)", dimless, 0.0)
+        dimensionedScalar("log_dbar", dimless, 0.0)
     ),
     sigma_hat_
     (
         IOobject
         (
-            "sigma hat",
+            "sigma_hat",
             mesh_.time().timeName(),
             mesh_,
             IOobject::NO_READ,
@@ -127,81 +127,69 @@ dMOM::dMOM
         dimLength,
         dMOMDict_.lookupOrDefault<scalar>("minDiameter", 1e-9))
 {
-    for (std::size_t i = 0; i < 3; ++i) {
-        Sgammas_.emplace_back
-        (
-            IOobject
-            (
-                "S" + std::to_string(i),
-                mesh_.time().timeName(),
+    forAll(Sgammas_, i) {
+        Sgammas_.set(i,
+                     new volScalarField(IOobject("S" + std::to_string(i),
+                                                 mesh_.time().timeName(),
+                                                 mesh_,
+                                                 IOobject::MUST_READ,
+                                                 IOobject::AUTO_WRITE),
+                                        mesh_));
+        breakupSource_.set(
+            i,
+            new volScalarField(
+                IOobject("breakup" + std::to_string(i) + "Source",
+                         mesh_.time().timeName(),
+                         mesh_,
+                         IOobject::NO_READ,
+                         IOobject::AUTO_WRITE),
                 mesh_,
-                IOobject::MUST_READ,
-                IOobject::AUTO_WRITE
-            ),
-            mesh_
-        );
-        breakupSource_.emplace_back
-        (
-            IOobject
-            (
-                "breakup" + std::to_string(i) + "Source",
-                mesh_.time().timeName(),
-                mesh_,
-                IOobject::NO_READ,
-                IOobject::AUTO_WRITE
-            ),
-            mesh_,
-            dimensionedScalar("0", Sgammas_[i].dimensions() / dimTime, 0.0)
-        );
+                dimensionedScalar(
+                    "0", Sgammas_[i].dimensions() / dimTime, 0.0)));
     }
+    printAvgMaxMin(mesh_, d());
 }
 
-dMOM::~dMOM()
-{
-}
+dMOM::~dMOM() {}
 
 using boost::math::normal;
 using boost::math::pdf;
 
-
-void dMOM::correct()
-{
+void dMOM::correct() {
     // Creating aliases to shorten notation
-    const volScalarField& S0 = Sgammas_[0];
-    const volScalarField& S1 = Sgammas_[1];
-    const volScalarField& S2 = Sgammas_[2];
+    const volScalarField &S0 = Sgammas_[0];
+    const volScalarField &S1 = Sgammas_[1];
+    const volScalarField &S2 = Sgammas_[2];
     // Calculate moments (note equation (2) in paper [1])
     const volScalarField m1 = S1 / S0;
     const volScalarField m2 = S2 / S0;
     // Non-logarithmized variance
     const volScalarField v = m2 - pow(m1, 2);
 
-    Info<< "updating size moments" << endl;
+    Info << "updating size moments" << endl;
     // Moment inversion is given analytically (see )
     log_d_bar_ = log(m1 / consistency_ / sqrt(1.0 + v / pow(m1, 2)));
     sigma_hat_ = sqrt(log(1.0 + v / pow(m1, 2)));
 
-    Info<< "Lognormal parameters:" << endl;
+    Info << "Lognormal parameters:" << endl;
     printAvgMaxMin(mesh_, S0);
     printAvgMaxMin(mesh_, log_d_bar_);
     printAvgMaxMin(mesh_, sigma_hat_);
 
     std::vector<volScalarField> mSources_;
-    const volScalarField& epsilon =
+    const volScalarField &epsilon =
         dispersedPhase_.U().mesh().lookupObject<volScalarField>(
-            "epsilon."+ dispersedPhase_.name());
-    const scalar& sigma = interfacialTension_;
+            "epsilon." + dispersedPhase_.name());
+    const scalar &sigma = interfacialTension_;
 
-    forAll(dispersedPhase_, celli)
-    {
+    forAll(dispersedPhase_, celli) {
         // Turbulent length scale
         auto L_k = pow(
-            pow(phase_.otherPhase().nu()()[celli], 4) / epsilon[celli],
-            0.25);
+            pow(phase_.otherPhase().nu()()[celli], 4) / epsilon[celli], 0.25);
 
-        auto shear_rate = sqrt(
-            phase_.otherPhase().rho()[celli] * epsilon[celli] /
-            phase_.otherPhase().mu()()[celli]);
+        auto shear_rate =
+            sqrt(phase_.otherPhase().rho()[celli] * epsilon[celli] /
+                 phase_.otherPhase().mu()()[celli]);
         // Critical diameter for viscous breakup
         auto d_cr_viscous =
             2 * sigma * Ca_cr_ / phase_.otherPhase().mu()()[celli] / shear_rate;
@@ -211,23 +199,23 @@ void dMOM::correct()
             (1 + C_alpha_) *
             pow(2 * sigma * We_cr_ / phase_.rho()[celli], 3.0 / 5.0) *
             pow(epsilon[celli], -2.0 / 5.0);
-        
+
         // Molecular viscosity ratio
         auto lambda = phase_.mu()()[celli] / phase_.otherPhase().mu()()[celli];
-        auto ftau = exp(
-            1.43 + 0.267 * log(lambda) - 0.023 * pow(log(lambda), 2));
+        auto ftau =
+            exp(1.43 + 0.267 * log(lambda) - 0.023 * pow(log(lambda), 2));
         auto tau_viscous_constant =
             (phase_.mu()()[celli] * shear_rate) / sigma * ftau;
         // Physical constants for inertial breakup
-        auto tau_inertia_constant = 2 * pi * k_br_ * sqrt(
-            (3 * phase_.rho()[celli] + 2 * phase_.otherPhase().rho()[celli]) /
-            (192 * sigma));
-
+        auto tau_inertia_constant =
+            2 * pi * k_br_ * sqrt((3 * phase_.rho()[celli] +
+                                   2 * phase_.otherPhase().rho()[celli]) /
+                                  (192 * sigma));
 
         // Modified distributions parameters
-        auto& std = sigma_hat_[celli];
+        auto &std = sigma_hat_[celli];
 
-        for(label gamma = 0; gamma < 3; ++gamma ) {
+        for (label gamma = 0; gamma < 3; ++gamma) {
             auto mu_viscous = log_d_bar_[celli] + (gamma - 1) * std;
             auto mu_inertial = log_d_bar_[celli] + (gamma - 1.5) * std;
             auto x_crit = log(d_cr_inertia);
@@ -238,113 +226,99 @@ void dMOM::correct()
             // exp expression are correction factor that results from the
             // x = log(d) substitution and recasting the integrals as erfc.
             // TODO: Needs more doc!
-            breakupSource_[gamma][celli] = 
+            breakupSource_[gamma][celli] =
                 // DPD constant contribution; the remaining d^gamma goes into
                 // correction factor
                 (pow(Nf_, (3 - gamma) / 3) - 1.0) *
                 (
                     // Viscous breakup
                     1 / tau_viscous_constant *
-                    (cdf(viscous, log(L_k)) - cdf(viscous, log(d_cr_viscous))) * 
-                    exp( 
-                        pow(gamma * std, 2) +
-                        2.0 * gamma * mu_viscous - 
-                        2.0 * gamma * pow(std, 2) - 
-                        2.0 * mu_viscous +
-                        pow(sigma, 2))
-                    +
+                        (cdf(viscous, log(L_k)) -
+                         cdf(viscous, log(d_cr_viscous))) *
+                        exp(pow(gamma * std, 2) + 2.0 * gamma * mu_viscous -
+                            2.0 * gamma * pow(std, 2) - 2.0 * mu_viscous +
+                            pow(sigma, 2)) +
                     // Inertial breakup contribution
-                    1 / tau_inertia_constant * 
-                    //TODO: does x_crit need limiting? 
-                    (1 - cdf(inertial, max(x_crit, log(L_k)))) * 
-                    exp( 
-                        pow(gamma * std, 2) +
-                        2.0 * gamma * mu_inertial - 
-                        3.0 * gamma * pow(std, 2) - 
-                        3.0 * mu_inertial +
-                        9.0 / 4.0 * pow(sigma, 2))
-                );
-
+                    1 / tau_inertia_constant *
+                        // TODO: does x_crit need limiting?
+                        (1 - cdf(inertial, max(x_crit, log(L_k)))) *
+                        exp(pow(gamma * std, 2) + 2.0 * gamma * mu_inertial -
+                            3.0 * gamma * pow(std, 2) - 3.0 * mu_inertial +
+                            9.0 / 4.0 * pow(sigma, 2)));
         }
     }
     for (auto bs : breakupSource_)
         printAvgMaxMin(mesh_, bs);
-    //TODO: is there a better way to assure that source terms on boundaries
-    //are equal to 0?
-    //maybe using internalField() instead of the whole field somewhere?
-    //how source terms are handled in combustion/chemistry/turbulence codes in
-    //OF
+    // TODO: is there a better way to assure that source terms on boundaries
+    // are equal to 0?
+    // maybe using internalField() instead of the whole field somewhere?
+    // how source terms are handled in combustion/chemistry/turbulence codes in
+    // OF
 
-    for (std::size_t i = 0; i < Sgammas_.size(); ++i)
-    {
-        fvScalarMatrix mEqn
-        (
-            fvm::ddt(Sgammas_[i])
-            + fvm::div(dispersedPhase_.phi(), Sgammas_[i])
-            //+ fvm::div(limitedFlux_,moments_[i])
-            ==
-            breakupSource_[i]
-        );
+    forAll (Sgammas_, i) {
+        fvScalarMatrix mEqn(fvm::ddt(Sgammas_[i]) +
+                                fvm::div(dispersedPhase_.phi(), Sgammas_[i])
+                            //+ fvm::div(limitedFlux_,moments_[i])
+                            == breakupSource_[i]);
         mEqn.relax();
         mEqn.solve();
     }
 
-    for (auto& moment : Sgammas_)
-    {
-        moment = max(
-            moment,
-            dimensionedScalar(moment.name(), moment.dimensions(), SMALL)
-        );
-        //TODO: print a warning message
+    for (auto &moment : Sgammas_) {
+        moment =
+            max(moment,
+                dimensionedScalar(moment.name(), moment.dimensions(), SMALL));
+        // TODO: print a warning message
         printAvgMaxMin(mesh_, moment);
     }
 
     printAvgMaxMin(mesh_, d());
 }
 
-const volScalarField dMOM::d() const
-{
-    // Diameter is assumed to be Sauter mean by following equation (6) in [1]
-    return max(6.0 / pi * dispersedPhase_ / Sgammas_[2], minDiameter_);
+const volScalarField dMOM::d() const {
+    // NOTE: There is some ciruclar logic in all Lo's models regarding the
+    // Sauter mean diameter calculation. All of his equations are valid if and
+    // only if the drops are exact spheres in which case, the Sauter mean should
+    // be exactly equal to mean diameter. A subtle difference comes only from
+    // the averaging process. mean(d**3) / mean(d**2) is not the same as
+    // mean(d). I do not think it is possible to estimate the difference using
+    // Jensen's type arguments.
+    //
+    // The expression below is used for thresholding the value so that small or
+    // zero phase fraction values do not compromise the stability of two fluid
+    // model (zero diameter limit).
+    return max(6.0 / pi * dispersedPhase_ /
+                   Sgammas_[2],            // Equatio (6) from paper [1]
+               Sgammas_[1] / Sgammas_[0]); // This could be user defined
+                                           // mean diameter, instead.
 }
 
 // * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
 
-tmp<volScalarField> dMOM::coalescenceSourceTerm(label momenti)
-{
-    volScalarField toReturn
-    (
-        IOobject
-        (
-            "Sbc",
-            mesh_.time().timeName(),
-            mesh_,
-            IOobject::NO_READ,
-            IOobject::NO_WRITE,
-            false
-        ),
+tmp<volScalarField> dMOM::coalescenceSourceTerm(label momenti) {
+    volScalarField toReturn(
+        IOobject("Sbc",
+                 mesh_.time().timeName(),
+                 mesh_,
+                 IOobject::NO_READ,
+                 IOobject::NO_WRITE,
+                 false),
         mesh_,
-        dimensionedScalar
-        (
-            "Sbc",
-            pow(dimVolume, momenti) /dimTime,
-            0
-        )
-    );
+        dimensionedScalar("Sbc", pow(dimVolume, momenti) / dimTime, 0));
 
-    const volScalarField& epsilon =
+    const volScalarField &epsilon =
         dispersedPhase_.U().mesh().lookupObject<volScalarField>(
-            "epsilon."+ dispersedPhase_.name());
+            "epsilon." + dispersedPhase_.name());
     const scalar sigma = phase_.fluid().sigma().value();
-    const label& gamma = momenti;
+    const label &gamma = momenti;
     forAll(dispersedPhase_, celli) {
 
-        //Equivalent diameter calculation
+        // Equivalent diameter calculation
         auto Sgamma = Sgammas_[gamma][celli];
-        //Paper[1] equation (5)
+        // Paper[1] equation (5)
         auto S3 = phase_[celli] * 6.0 / pi;
-        //Paper[1] equation (3)
-        auto d3gamma = pow(S3 / Sgamma, 1 / (3 - gamma)); 
+        // Paper[1] equation (3)
+        auto d3gamma = pow(S3 / Sgamma, 1 / (3 - gamma));
         auto d_eq = k_cl_1_ * d3gamma;
 
         // TODO: My guess is that here a switching MUST occur between inertial
@@ -352,71 +326,51 @@ tmp<volScalarField> dMOM::coalescenceSourceTerm(label momenti)
 
         // *** Viscous breakup ***
         auto k_coll = sqrt(8 * pi / 3);
-        auto shear_rate = sqrt(
-            phase_.otherPhase().rho()[celli] * epsilon[celli] /
-            phase_.otherPhase().mu()()[celli]);
+        auto shear_rate =
+            sqrt(phase_.otherPhase().rho()[celli] * epsilon[celli] /
+                 phase_.otherPhase().mu()()[celli]);
         auto u_rel = shear_rate * d_eq;
         // *** Viscous probability of coalescence ***
-        
+
         // Hamaker constant; given in paper [2] after in the comment to eq 46
         auto A_H = 5e-21;
         // Critical film thickness eq (46) in paper [2]
         auto h_cr = pow(A_H * d_eq / (24.0 * pi * sigma), 1 / 3);
 
-        //Interaction force
-        auto F_i =
-            3.0 * pi / 2.0 * phase_.otherPhase().mu()()[celli] *
-            shear_rate * pow(d_eq, 2);
+        // Interaction force
+        auto F_i = 3.0 * pi / 2.0 * phase_.otherPhase().mu()()[celli] *
+                   shear_rate * pow(d_eq, 2);
 
         // Finally, ladies and gentlemen, I present to you: Drainage model 3!
-        auto td = 
-            pi * phase_.mu()()[celli] * sqrt(F_i) /
-            (2 * h_cr) *
-            pow(d_eq / (4 * pi * sigma), 3.0 / 2.0);
-        
-        //Equation (40)
+        auto td = pi * phase_.mu()()[celli] * sqrt(F_i) / (2 * h_cr) *
+                  pow(d_eq / (4 * pi * sigma), 3.0 / 2.0);
+
+        // Equation (40)
         auto P_coal = exp(-td * shear_rate);
 
-        toReturn[celli] = 
-            (pow(2.0, gamma / 3) - 2.0) *
-            pow(6.0 * phase_[celli] / pi, 2.0) *
-            k_coll *
-            u_rel *
-            P_coal *
-            pow(d_eq, gamma - 4);
+        toReturn[celli] = (pow(2.0, gamma / 3) - 2.0) *
+                          pow(6.0 * phase_[celli] / pi, 2.0) * k_coll * u_rel *
+                          P_coal * pow(d_eq, gamma - 4);
     }
 
-    return tmp<volScalarField>( new volScalarField(toReturn));
+    return tmp<volScalarField>(new volScalarField(toReturn));
 }
 
-tmp<volScalarField> dMOM::breakupSourceTerm(label momenti)
-{
-    volScalarField toReturn
-    (
-        IOobject
-        (
-            "Sbr",
-            mesh_.time().timeName(),
-            mesh_,
-            IOobject::NO_READ,
-            IOobject::NO_WRITE,
-            false
-        ),
+tmp<volScalarField> dMOM::breakupSourceTerm(label momenti) {
+    volScalarField toReturn(
+        IOobject("Sbr",
+                 mesh_.time().timeName(),
+                 mesh_,
+                 IOobject::NO_READ,
+                 IOobject::NO_WRITE,
+                 false),
         mesh_,
-        dimensionedScalar
-        (
-            "Sbr", 
-            pow(dimVolume, momenti) / dimTime,
-            0
-        ) 
-    );
+        dimensionedScalar("Sbr", pow(dimVolume, momenti) / dimTime, 0));
 
-
-    return tmp<volScalarField>( new volScalarField(toReturn));
+    return tmp<volScalarField>(new volScalarField(toReturn));
 }
 
-
-}//end namespace PBEMethods
-}//end namespace Foam
+} // end namespace PBEMethods
+} // end namespace Foam
 
 // ************************************************************************* //
